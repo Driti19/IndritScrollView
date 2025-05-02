e228635a0930e202961b4228de393672386aadb9
Driti19 <ndricimtsh69@gmail.com>
2025-05-02 14:18:56 -0400

    done

--- IndritScrollView/App.js
+++ IndritScrollView/App.js
@@ -5,6 +5,8 @@ import {
   StyleSheet,
   ScrollView,
   TouchableOpacity,
+  alignItems,
+  justifyContent,
 } from 'react-native';
 // import { Constants } from 'expo';
 
@@ -12,22 +14,28 @@ const App = () =>  {
     return (
       <View style={styles.root}>
 
-        <View style={styles.redsection2}></View>
+        <View style={styles.redsection2}><Text>navbar</Text></View>
        
         <View style={styles.root2}>
           <ScrollView contentContainerStyle={{ height: 2400 }}>
             
             <View style={styles.box2}>
-            <Text>navbar</Text>
+            <View style={styles.containerb}>
+  
+  <View style={styles.subcontainer1b} />
+  <View style={styles.subcontainer2b} />
+</View>
             </View>
-            <View style={styles.box1}></View>
+            <View style={styles.box1}><Text>Section 1</Text></View>
             <View style={styles.box2}></View>
             <View style={styles.box1}></View>
           </ScrollView>
         </View>
 
         
-           <View style={styles.redsection}></View>
+           <View style={styles.redsection}><Text>footer</Text></View>
+
+
            
         </View>
     );
@@ -48,18 +56,36 @@ const styles = StyleSheet.create({
   box1: {
     height: 100,
     backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   box2: {
     height: 100,
     backgroundColor: 'purple',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 
   redsection: {
-    height: 200,
+    height: 100,
     backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   redsection2: {
-    height: 200,
+    height: 100,
     backgroundColor: 'lime',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  containerb: { flex: 1, flexDirection: 'row' },
+
+  subcontainer1b: {
+    flex: .5,
+    backgroundColor: 'powderblue',
+  },
+  subcontainer2b: {
+    flex: .5,
+    backgroundColor: 'green',
   },
 });
